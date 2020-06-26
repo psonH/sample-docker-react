fdbf7ed57d2bdc4e81a7d9c5614dec31abbad800
psonH <p47.hajela@gmail.com>
2020-06-26 23:18:39 +0530

    build files

---
+++ build/precache-manifest.46b169b02c9f730b1eacd35d8567a595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63042ffa77a9227d677a61c1260e092d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5757fcd003772f7ede7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ae91f31304bfac2cd79",
+    "url": "/static/js/2.8b0193bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b0193bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5757fcd003772f7ede7",
+    "url": "/static/js/main.6ee83a9b.chunk.js"
+  },
+  {
+    "revision": "d29e7945567353dd8bd6",
+    "url": "/static/js/runtime-main.1f840449.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
